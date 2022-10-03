1f22c6d0eab1d9b38079a1b445cc7a2d485da1e4
JHONASPRILLA <jhonarroz9@gmial.com>
2022-10-03 00:02:10 -0500

    portafolio comienzo

---
+++ script/dinamico.js
@@ -0,0 +1,54 @@
+
+window.sr= ScrollReveal();
+
+
+
+sr.reveal('.scroll-1',{
+    duration: 1000,
+    origin:'top',
+    distance:'-500px',
+    delay:600,
+});
+
+sr.reveal('.scroll-2',{
+    duration: 1000,
+    origin:'top',
+    distance:'-500px',
+    delay:600,
+});
+
+sr.reveal('.scroll-3',{
+    duration: 1000,
+    origin:'top',
+    distance:'-500px',
+    delay:600,
+});
+
+sr.reveal('.foto',{
+    duration: 3000,
+    origin:'rigth',
+    distance:'-1000px',
+    delay:600,
+});
+
+ScrollReveal().reveal('.foto', {
+	interval: 0,
+	reset: true
+});
+
+
+ScrollReveal().reveal('.scroll-1', {
+	interval: 5,
+	reset: true
+});
+
+ScrollReveal().reveal('.scroll-2', {
+	interval: 5,
+	reset: true
+});
+
+ScrollReveal().reveal('.scroll-3', {
+	interval: 5,
+	reset: true
+});
+
